cd4b5a4515e3b0a495795a05c21fa492efb3c096
DisquietingFridge <30654622+DisquietingFridge@users.noreply.github.com>
2022-08-06 02:03:03 +1000

    -Broke out grid edge rule objects into separate cpp and header files - Finished Sphere grid rule implementation

--- Source/MyProject/AutomataDriver.cpp
+++ Source/MyProject/AutomataDriver.cpp
@@ -2,7 +2,6 @@
 #include "Materials/MaterialParameterCollectionInstance.h"
 #include "Misc/Char.h"
 #include "Async/Async.h"
-#include "Algo/Accumulate.h"
 #include "NiagaraFunctionLibrary.h"
 #include "NiagaraDataInterfaceArrayFloat.h"
 #include "NiagaraDataInterfaceArrayFunctionLibrary.h"
@@ -115,7 +114,13 @@ void AAutomataDriver::InitializeMaterial()
 
 void AAutomataDriver::InitializeCellTransforms()
 {
+	using namespace HexCoords;
 	CellTransforms.Reserve(NumCells());
+
+	auto VectorConversion = [&](FVector2D Point)
+	{
+		return Offset * FVector(Point[0], 0, Point[1]);
+	};
 
 	for (int i = 0; i < NumCells(); ++i)
 	{
@@ -124,11 +129,11 @@ void AAutomataDriver::InitializeCellTransforms()
 		switch (Shape)
 		{
 		case CellShape::Square:
-			TransformResult = Offset * FVector(Coord[0], 0, Coord[1]);
+			TransformResult = VectorConversion(Coord);
 			break;
 		case CellShape::Hex:
-			FVector2D TwoDeeTransform = HexCoords::OffsetToTransform(Coord);
-			TransformResult = Offset * FVector(TwoDeeTransform[0], 0, TwoDeeTransform[1]);
+			TransformResult = VectorConversion(OffsetToTransform(Coord));
+			break;
 		}
 		CellTransforms.Add(TransformResult);
 	}
@@ -195,6 +200,8 @@ void AAutomataDriver::CreateGridRuleInterface()
 
 void AAutomataDriver::InitializeCellNeighborhoods()
 {
+	using namespace HexCoords;
+
 	Neighborhoods.Init(nullptr, NumCells());
 
 	ParallelFor(NumCells(), [&](int CellID)
@@ -203,26 +210,24 @@ void AAutomataDriver::InitializeCellNeighborhoods()
 		NeighborInfo.Reserve(RelativeNeighborhood.Num());
 
 		TArray<FIntPoint> NeighborCoords = RelativeNeighborhood;
-		FIntPoint CellCoords = GridCoords[CellID];
+		FIntPoint CellCoord = GridCoords[CellID];
 
 		switch (Shape)
 		{
 		case (CellShape::Square):
 			for (int i = 0; i < RelativeNeighborhood.Num(); ++i)
 			{
-				NeighborCoords[i] += CellCoords;
+				NeighborCoords[i] += CellCoord;
 				NeighborInfo.Add(TPair<FIntPoint, FIntPoint>(RelativeNeighborhood[i], NeighborCoords[i]));
 			}
 			break;
 
 		case (CellShape::Hex):
-			FIntPoint AxialCoord = HexCoords::OffsetToAxial(CellCoords);
+			FIntPoint AxialCoord = OffsetToAxial(CellCoord);
 			for (int i = 0; i < RelativeNeighborhood.Num(); ++i)
 			{
 				NeighborCoords[i] += AxialCoord;
-				FIntPoint OffsetPoint = HexCoords::AxialToOffset(NeighborCoords[i]);
-				NeighborCoords[i] = OffsetPoint;
-
+				NeighborCoords[i] = AxialToOffset(NeighborCoords[i]);
 				NeighborInfo.Add(TPair<FIntPoint, FIntPoint>(RelativeNeighborhood[i], NeighborCoords[i]));
 			}
 			break;
@@ -278,7 +283,6 @@ void AAutomataDriver::ApplyCellRules()
 {
 	ParallelFor(NumCells(), [&](int32 CellID)
 	{
-
 		if (EvalFlaggedLastStep[CellID])
 		{
 			int AliveNeighbors = GetCellAliveNeighbors(CellID);
@@ -337,7 +341,6 @@ void AAutomataDriver::TimestepPropertyShift()
 	ParallelFor(NumCells(), [&](int32 CellID)
 	{
 		CurrentStates[CellID] = NextStates[CellID];
-
 		EvalFlaggedLastStep[CellID] = EvalFlaggedThisStep[CellID];
 		EvalFlaggedThisStep[CellID] = false;
 
@@ -349,213 +352,3 @@ void AAutomataDriver::TimerFired()
 	StepComplete();
 }
 
-UStandardXZGrid::UStandardXZGrid()
-{
-	IGridSpecsInterface* Grid = Cast<IGridSpecsInterface>(GetOuter());
-	if (Grid)
-	{
-		Tie(NumXCells, NumZCells) = Grid->GetGridDimensions();
-	}
-}
-
-int UStandardXZGrid::ApplyRule(FIntPoint Coord) const
-{
-	return -1;
-}
-
-TSharedPtr<TArray<int>> UStandardXZGrid::RawCoordsToCellIDs(TArray<FIntPoint>& RawCoords) const
-{
-	TArray<int> CellIDs;
-	for (FIntPoint Coord : RawCoords)
-	{
-		int CellID = ApplyRule(Coord);
-		if (CellID >= 0)
-		{
-			CellIDs.Add(CellID);
-		}
-	}
-	TSharedPtr<TArray<int>> Pointer = MakeShared<TArray<int>>(CellIDs);
-	return Pointer;
-}
-
-TSharedPtr<TMap<FIntPoint, int>> UStandardXZGrid::MapNeighborhood(TArray<TPair<FIntPoint, FIntPoint>> NeighborInfo) const
-{
-	TMap<FIntPoint,int> NeighborhoodMap;
-
-	for (int i = 0 ; i < NeighborInfo.Num(); ++i)
-	{
-		FIntPoint RawCoord = NeighborInfo[i].Value;
-		int CellID = ApplyRule(RawCoord);
-		if (CellID >= 0)
-		{
-			FIntPoint Key = NeighborInfo[i].Key;
-			NeighborhoodMap.Add(Key, CellID);
-		}
-	}
-	TSharedPtr<TMap<FIntPoint, int>> Pointer = MakeShared<TMap<FIntPoint, int>>(NeighborhoodMap);
-	return Pointer;
-
-}
-
-bool UStandardXZGrid::IsAxisTwisted(FIntPoint Coord, DeformedAxis TwistedAxis) const
-{
-	int NumAxisCells = 0;
-	int Index = 0;
-
-	switch (TwistedAxis)
-	{
-	case DeformedAxis::XAxis:
-		NumAxisCells = NumXCells;
-		Index = 0;
-		break;
-	case DeformedAxis::ZAxis:
-		NumAxisCells = NumZCells;
-		Index = 1;
-		break;
-	}
-
-	if (Coord[Index] < 0)
-	{
-		return !(bool)(((abs(Coord[Index]) - 1) / NumAxisCells) % 2);
-	}
-	else
-	{
-		return bool((Coord[Index] / NumAxisCells) % 2);
-	}
-}
-
-void UStandardXZGrid::ParamsFromAxis(int& Index, int& NumAxisCells, DeformedAxis Axis) const
-{
-	switch (Axis)
-	{
-	case DeformedAxis::XAxis:
-		NumAxisCells = NumXCells;
-		Index = 0;
-		break;
-	case DeformedAxis::ZAxis:
-		NumAxisCells = NumZCells;
-		Index = 1;
-		break;
-	}
-}
-
-void UStandardXZGrid::LoopAxis(FIntPoint& Coord, DeformedAxis AxisToLoop) const
-{
-	int NumAxisCells;
-	int Index;
-	ParamsFromAxis(Index, NumAxisCells, AxisToLoop);
-
-	if (Coord[Index] >= 0)
-	{
-		Coord[Index] = Coord[Index] % NumAxisCells;
-	}
-	else
-	{
-		Coord[Index] = NumAxisCells - (abs(Coord[Index]) % NumAxisCells);
-	}
-}
-
-void UStandardXZGrid::ReverseAxis(FIntPoint& Coord, DeformedAxis AxisToReverse) const
-{
-	int NumAxisCells = 0;
-	int Index = 0;
-	ParamsFromAxis(Index, NumAxisCells, AxisToReverse);
-
-	LoopAxis(Coord, AxisToReverse);
-	Coord[Index] = NumAxisCells - Coord[Index] - 1;
-}
-
-int USphereRule::ApplyRule(FIntPoint Coord) const
-{
-	return -1;
-}
-
-int UCrossSurfaceRule::ApplyRule(FIntPoint Coord) const
-{
-
-	bool ZAxisTwisted = IsAxisTwisted(Coord, DeformedAxis::ZAxis);
-	bool XAxisTwisted = IsAxisTwisted(Coord, DeformedAxis::XAxis);
-
-	if (XAxisTwisted)
-	{
-		ReverseAxis(Coord, DeformedAxis::ZAxis);
-	}
-	else
-	{
-		LoopAxis(Coord, DeformedAxis::ZAxis);
-	}
-
-	if (ZAxisTwisted)
-	{
-		ReverseAxis(Coord, DeformedAxis::XAxis);
-	}
-	else
-	{
-		LoopAxis(Coord, DeformedAxis::XAxis);
-	}
-
-	return CoordToCellID(Coord);
-}
-
-int UKleinRule::ApplyRule(FIntPoint Coord) const
-{
-
-	bool ZAxisTwisted = IsAxisTwisted(Coord, DeformedAxis::ZAxis);
-
-	if (ZAxisTwisted)
-	{
-		ReverseAxis(Coord, DeformedAxis::XAxis);
-	}
-	else
-	{
-		LoopAxis(Coord, DeformedAxis::XAxis);
-	}
-
-	LoopAxis(Coord, DeformedAxis::ZAxis);
-
-	return CoordToCellID(Coord);
-
-}
-
-int UTorusRule::ApplyRule(FIntPoint Coord) const
-{
-	LoopAxis(Coord, DeformedAxis::XAxis);
-	LoopAxis(Coord, DeformedAxis::ZAxis);
-
-	return CoordToCellID(Coord);
-}
-
-int UCylinderRule::ApplyRule(FIntPoint Coord) const
-{
-	if ((Coord[1] >= 0) && (Coord[1] < NumZCells))
-	{
-		LoopAxis(Coord, DeformedAxis::XAxis);
-
-		return CoordToCellID(Coord);
-	}
-	else return -1;
-}
-
-int UFiniteRule::ApplyRule(FIntPoint Coord) const
-{
-	int x = Coord[0];
-	int z = Coord[1];
-
-	if (((x >= 0) && (z >= 0)) && ((x < NumXCells) && (z < NumZCells)))
-	{
-		return CoordToCellID(Coord);
-	}
-	else return -1;
-}
-
-IGridRuleInterface* UBaseGridRuleFactory::CreateGridRuleInterface(BoundGridRuleset Ruleset)
-{
-	const TSubclassOf<UStandardXZGrid>* RuleTypeResult = RuleEnumToClass.Find(Ruleset);
-	TSubclassOf<class UStandardXZGrid> RuleType = RuleTypeResult ? *RuleTypeResult : UStandardXZGrid::StaticClass();
-
-	UObject* CreatedObject = NewObject<UStandardXZGrid>(this->GetOuter(), RuleType);
-
-	IGridRuleInterface* GridRule;
-	GridRule = Cast<IGridRuleInterface>(CreatedObject);
-	return GridRule;
-}
--- Source/MyProject/AutomataDriver.h
+++ Source/MyProject/AutomataDriver.h
@@ -1,126 +1,14 @@
 #pragma once
 
-#include "CoreMinimal.h"
-#include "GridRuleInterface.h"
+#include "GridEdgeRules.h"
 #include "NiagaraFunctionLibrary.h"
 #include "NiagaraComponent.h"
 #include "NiagaraDataInterfaceArrayFloat.h"
 #include "AutomataDriver.generated.h"
 
-UCLASS()
-class UStandardXZGrid : public UObject, public IGridRuleInterface
-{
-	GENERATED_BODY()
 
-protected:
-	int NumXCells;
-	int NumZCells;
 
-public:
 
-	UStandardXZGrid();
-
-	virtual int ApplyRule(FIntPoint Coord) const;
-
-	virtual TSharedPtr<TArray<int>> RawCoordsToCellIDs(TArray<FIntPoint>& RawCoords) const override;
-
-	virtual TSharedPtr<TMap<FIntPoint, int>> MapNeighborhood(TArray<TPair<FIntPoint, FIntPoint>> NeighborInfo) const override;
-
-	bool IsAxisTwisted(FIntPoint Coord, DeformedAxis TwistedAxis) const;
-
-	void ParamsFromAxis(int& Index, int& NumAxisCells, DeformedAxis Axis) const;
-
-	virtual void LoopAxis(FIntPoint& Coord, DeformedAxis AxisToLoop) const;
-
-	virtual void ReverseAxis(FIntPoint& Coord, DeformedAxis AxisToReverse) const;
-
-	int CoordToCellID(FIntPoint Coord) const
-	{
-		return ((Coord[1] * NumXCells) + Coord[0]);
-	}
-};
-
-UCLASS()
-class USphereRule : public UStandardXZGrid
-{
-	GENERATED_BODY()
-
-public:
-
-	virtual int ApplyRule(FIntPoint Coord) const override;
-};
-
-UCLASS()
-class UCrossSurfaceRule : public UStandardXZGrid
-{
-	GENERATED_BODY()
-
-public:
-
-	virtual int ApplyRule(FIntPoint Coord) const override;
-};
-
-UCLASS()
-class UKleinRule : public UStandardXZGrid
-{
-	GENERATED_BODY()
-
-public:
-
-	virtual int ApplyRule(FIntPoint Coord) const override;
-
-};
-
-UCLASS()
-class UTorusRule : public UStandardXZGrid
-{
-	GENERATED_BODY()
-
-public:
-
-	virtual int ApplyRule(FIntPoint Coord) const override;
-
-};
-
-UCLASS()
-class UCylinderRule : public UStandardXZGrid
-{
-	GENERATED_BODY()
-
-public:
-
-	virtual int ApplyRule(FIntPoint Coord) const override;
-};
-
-UCLASS()
-class UFiniteRule : public UStandardXZGrid
-{
-	GENERATED_BODY()
-
-public:
-
-	virtual int ApplyRule(FIntPoint Coord) const override;
-};
-
-const TMap<BoundGridRuleset, TSubclassOf<UStandardXZGrid>> RuleEnumToClass
-= {
-	{BoundGridRuleset::Finite, UFiniteRule::StaticClass()},
-	{BoundGridRuleset::Cylinder, UCylinderRule::StaticClass()},
-	{BoundGridRuleset::Torus, UTorusRule::StaticClass()},
-	{BoundGridRuleset::Klein, UKleinRule::StaticClass()},
-	{BoundGridRuleset::CrossSurface, UCrossSurfaceRule::StaticClass()},
-	{BoundGridRuleset::Sphere, USphereRule::StaticClass()}
-};
-
-UCLASS()
-
-class UBaseGridRuleFactory : public UObject, public IGridRuleFactoryInterface
-{
-	GENERATED_BODY()
-
-public:
-	virtual IGridRuleInterface* CreateGridRuleInterface(BoundGridRuleset Ruleset) override;
-};
 
 const TArray<FIntPoint> RelativeMooreNeighborhood
 {
